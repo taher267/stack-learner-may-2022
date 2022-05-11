48c00862228ed2f6c72eb3aa6782ae49570c831b
ABU TAHER <abutaher267@gmail.com>
2022-05-11 12:16:15 +0600

    Iterator

---
+++ Full Stack Army/Async-Iterator-and-Generator-c-11/app.js
@@ -0,0 +1,50 @@
+// const arr = [1, 2, 3, 4];
+// for (i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+/**
+ * nessecity of iterator
+ */
+// let index = 0;
+// function next() {
+//     return arr[index++]
+// }
+// console.log(next());
+// console.log('text');
+// console.log('text');
+// console.log(next());
+
+/**
+ * Iterator
+ *
+ */
+// const range = {
+//     start: 0,
+//     stop: 20,
+//     step: 5
+// }
+
+// range[Symbol.iterator] = function () {
+//     let current = this.start;
+//     const stop = this.stop;
+//     const step = this.step;
+//     return {
+//         next() {
+//             const o = {
+//                 value: current,
+//                 done: current > stop
+//             }
+//             current += step
+//             return o
+//         }
+//     }
+// }
+// const iterRanger = range[Symbol.iterator]();
+
+
+// for (let v of range) {
+//     console.log(v);
+// }
+
+
--- Full Stack Army/condition/app.js
+++ Full Stack Army/condition/app.js
@@ -1,1 +1,24 @@
-// #Full Stack Army
+// //Condition
+// if (studyBasic) {
+//     wontJoin()
+// }
+// if (studyAdvanced) {
+//     join()
+// }
+// if (teacherSpeaks) {
+//     slient()
+// }
+// if (!teacherSpeaks) {
+//     shout()
+// }
+// //Scenario 1 Single branch
+// // if condition
+// if (hasMany) {
+//     purchasePhone
+// }
+
+// //Scenario 1 Single branch
+// // if condition
+// if (hasMany) {
+//     purchasePhone
+// }
